02855f1ba7fa634ab3f4279794ad3e8e12045c37
myngoc-trg <ngocnancy1807@gmail.com>
2026-04-08 14:32:04 +0200

    Completed assignment.

--- A1N_Chatbot/student/src/Chatterbot.hs
+++ A1N_Chatbot/student/src/Chatterbot.hs
@@ -6,6 +6,8 @@ import Data.Maybe
 
 -- If you're not sure what this is, it's ok.
 import Control.Monad (mapM)
+import qualified Data.Either as E
+import Data.Data (ConstrRep(FloatConstr))
 
 -- A pattern is a list of things
 -- Where we have either a value or a wildcard
@@ -19,7 +21,7 @@ newtype Pattern a = Pattern [PatternElem a]
 -- Templates are the same as patterns
 type Template a = Pattern a
 
--- A phrase is a list of string
+-- A phrase is a list of string, list of words
 type Phrase = [String]
 
 -- One rule contains: 
@@ -60,19 +62,30 @@ stateOfMind :: BotBrain -> IO (Phrase -> Phrase)
 stateOfMind b =
   fmap rulesApply (mapM makePair b)
 
+
 -- A rule maps a pattern to many answers, so we choose one
 -- at random, and that's our bot
 makePair :: Rule -> IO (Pattern String, Template String)
-{- TO BE WRITTEN -}
-makePair = undefined
+makePair (Rule (pat, templates)) = do
+  r <- randomIO :: IO Float
+  return (pat, pick r templates)
+--makePair = undefined
 
+
+-- try :: (a -> Maybe a) -> a -> a
+-- try f x = maybe x id (f x)
+-- i.e. if f x returns Just y, use y
+  -- if f x fails with Nothing, keep original x
 rulesApply :: [(Pattern String, Template String)] -> Phrase -> Phrase
-{- TO BE WRITTEN -}
-rulesApply = undefined
+rulesApply rules = try(transformationsApply reflect rules)
+-- transformationsApply reflect rules :: Phrase -> Maybe Phrase
 
+-- lookup :: Eq a => a -> [(a,b)] -> Maybe b
+-- map applies the function to every element in the list
+-- map :: (a -> b) -> [a] -> [b]
 reflect :: Phrase -> Phrase
-{- TO BE WRITTEN -}
-reflect = undefined
+reflect = map reflectWord 
+  where reflectWord w = fromMaybe w (lookup w reflections)
 
 reflections =
   [ ("am",     "are"),
@@ -102,6 +115,8 @@ endOfDialog = (=="quit") . map toLower
 present :: Phrase -> String
 present = unwords
 
+-- remove punctuation characters with filter(...)
+--
 prepare :: String -> Phrase
 prepare = reduce . words . map toLower . filter (not . flip elem ".,:;*!#%&|")
 
@@ -109,8 +124,9 @@ rulesCompile :: [(String, [String])] -> BotBrain
 rulesCompile = map ruleCompile
 
 ruleCompile :: (String, [String]) -> Rule
-{- TO BE WRITTEN -}
-ruleCompile = undefined
+ruleCompile (pat, templates) =
+  Rule (starPattern (map toLower pat), map starPattern templates)
+--ruleCompile = undefined
 
 --------------------------------------
 
@@ -127,7 +143,9 @@ mkPattern wc = Pattern . map toPatternElem
 --mkPattern = undefined
 
 stringToPattern :: String -> String -> Pattern String
-stringToPattern wc = mkPattern wc . words
+stringToPattern wc = mkPattern wc . words 
+-- words :: String -> [String] (converting string to phrase - list of words)
+-- then make pattern wc (wildCard) [String]
 
 starPattern :: String -> Pattern String
 starPattern = stringToPattern "*"
@@ -150,9 +168,14 @@ reductions = (map . map2) (starPattern, starPattern)
 reduce :: Phrase -> Phrase
 reduce = reductionsApply reductions
 
+
+--fix :: Eq a => (a -> a) -> a -> a
+--fix f x
+--   |  f x == x  = x
+--   |  otherwise = fix f (f x)
+-- keep applying f until the result stops changing
 reductionsApply :: [(Pattern String, Pattern String)] -> Phrase -> Phrase
-{- TO BE WRITTEN -}
-reductionsApply = undefined
+reductionsApply reductions = fix(try(transformationsApply id reductions))
 
 
 -------------------------------------------------------
@@ -226,13 +249,37 @@ longerWildcardMatch _ _ = Nothing
 -- Helper function: Matches a pattern and applies the transformation
 matchAndTransform :: Eq a => ([a] -> [a]) -> Pattern a -> [a] -> Maybe [a]
 matchAndTransform transform pat = (mmap transform) . (match pat)
+-- ((mmap transform) . (match pat)) x is equivalent to mmap transform  (match pat x)
+-- map f x so first match pat with [a]
+-- if the results of match pat [a] is Nothing then Nothing
+-- if rhe results of match pat [a] is Just x then Just (f x), meaning apply transform to that result
+-- transform is a function from lists to lists
 
 -- Applying a single pattern
-transformationApply :: Eq a => ([a] -> [a]) -> [a] -> (Pattern a, Template a) -> Maybe [a]
-{- TO BE WRITTEN -}
-transformationApply = undefined
+-- first argumetn: ([a] -> [a]), is a function from lists to lists (e.g. id, reflect,...)
+-- second argument: [a], is the input string/list we want to match
+-- third argument: (Pattern a, Template a)
+transformationApply :: Eq a => 
+  ([a] -> [a]) -> -- function to apply to extracted match
+  [a] -> -- string to match
+  (Pattern a, Template a) -> -- the transformation
+  Maybe [a] -- the result
+transformationApply transform input (pat, template) = 
+  mmap (substitute template) (matchAndTransform transform pat input)
+-- i.e
+-- case matchAndTransform transform pat input of
+--    Nothing -> Nothing
+--    Just xs -> Just (substitute template xs)
+
 
 -- Applying a list of patterns until one succeeds
-transformationsApply :: Eq a => ([a] -> [a]) -> [(Pattern a, Template a)] -> [a] -> Maybe [a]
-{- TO BE WRITTEN -}
-transformationsApply = undefined
+transformationsApply :: Eq a => 
+  ([a] -> [a]) -> -- transform function to apply to extracted match
+  [(Pattern a, Template a)] -> -- list of transformations
+  [a] -> -- string to match
+  Maybe [a] -- result
+transformationsApply _ [] _ = Nothing
+transformationsApply transform (t:ts) input =
+  transformationApply transform input t `orElse`
+  transformationsApply transform ts input
+--transformationsApply = undefined
